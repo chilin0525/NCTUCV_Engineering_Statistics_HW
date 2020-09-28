e906f6f20d7e5afc4575ab04b177e9397e8da1fb
7070707070 <sky2502211@gmail.com>
2020-09-28 10:46:10 +0800

    hw1 done

--- 0711282_HW1_p1.r
+++ 0711282_HW1_p1.r
@@ -9,6 +9,8 @@ print(sd(top_ten_score))
 
 pass_num = 0
 id=0
+max_score = 0
+min_score = 0
 pass_student <- vector()
 for(i in statis.score){
     id <- id + 1
@@ -16,6 +18,17 @@ for(i in statis.score){
         pass_num <- pass_num + 1
         pass_student <- c(pass_student,i)
     }
+    if(i>max_score) max_score <- i
+    if(i<min_score) min_score <- i
 }
 print(pass_num)
 print(pass_student)
+print(min_score)
+print(max_score)
+
+tmp <- sort(statis.score,decreasing=TRUE)
+sort_score <- tmp[c(1:10)]
+mean(sort_score)
+sd(sort_score)
+
+summary(statis.score)
