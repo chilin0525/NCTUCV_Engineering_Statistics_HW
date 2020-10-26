ea444147f17f8879a653b7a1c09c5dc584c16722
7070707070 <sky2502211@gmail.com>
2020-10-26 11:24:55 +0800

    donw week5

--- test.cpp
+++ test.cpp
@@ -4,23 +4,13 @@ using namespace std;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     
-    int a[1000];
-    vector<int> ans;
-    fill(a,a+1000,1);
-    a[0]=0;
-    a[1]=0;
-    for(int i=2;i<1000;i++){
-        if(a[i]){
-            ans.push_back(i);
-            for(int j=i+i;j<1000;j+=i){
-                    a[j]=0;
-            }
-        }
-    }
+    int x=1;
+    if(true)
+        if(false)
+            x=2;
+        else
+            x=3;
 
-    for(auto &t:ans){
-        cout<<t<<" ";
-    }
-    
+    cout<<x<<endl;
     return 0;
 }
--- week4/0711282_邱頎霖_Prog1-1.r
+++ week4/0711282_邱頎霖_Prog1-1.r
@@ -1,11 +1,13 @@
 num = rep(1,1005)
 ans <- vector()
 num[1] = 0
+total=0
 
 check_prime <- function(){
     i = 2
     while(i<1000){
         if(num[i]){
+            total = total + 1
             #ans <- c(ans,i)
             cat(i,sep="\n")
             j=i+i
@@ -16,9 +18,13 @@ check_prime <- function(){
         }
         i = i+1
     }
+    return(total)
 }
 
 sink("prime.txt")
 cat("There are prime number:",sep="\n")
-check_prime()
+total <- check_prime()
 sink()
+
+cat("total:")
+cat(total)
