c9846496298fcb50a568f897d311f7fc5f19de5f
7070707070 <sky2502211@gmail.com>
2020-12-07 11:32:17 +0800

    hw9 done

--- HW9/0711282-邱頎霖-ch2-1.R
+++ HW9/0711282-邱頎霖-ch2-1.R
@@ -8,14 +8,49 @@ for(i in times){
     print(v1)
     flag = 0;    
     for(j in v1){
-        if(j<=9){
-            flag=1;
-        } 
+        if(j<=9){flag=1;} 
     }
     if(!flag) rec = rec + 1
 }
-print("number of no any typhoon:")
+cat("number of no any typhoon:\n")
 print(rec)
+cat("prob:\n")
+print(rec/8)
 
 cat("\n")
 
+times <- c(4:15)
+ans <- c()
+ans_times <- c()
+
+for(i in times){
+    tmp = c(1:2^i)
+    rec2 = 0;
+    for(k in tmp){
+        x = sample(0:99,20)
+        v1 = unlist(x)
+        # convert list to vector
+
+        flag = 0;    
+        for(j in v1){
+            if(j<=9){flag=1;} 
+        }
+        if(!flag) rec2 = rec2 + 1
+    }
+    print("number of no any typhoon:")
+    print(rec2)
+    cat("prob:\n")
+    print(rec2/(2^i))
+    ans <- append(ans,rec2/(2^i))
+    ans_times <- append(ans_times,(2^i))
+}
+
+cat("\n")
+print(ans)
+print(ans_times)
+# prob of every times
+
+par(mfrow=c(1,1))
+plot(ans_times,ans,type="l",ylim=c(0,0.25))
+abline(h=0.1,col="blue")
+points(ans_times,ans)
